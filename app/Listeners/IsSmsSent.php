3a248b648424088aac08d2c4dec00bf615b2c03d
Devlangat <kevokipa4@gmail.com>
2022-04-02 08:30:56 +0300

    first commit

---
+++ app/Listeners/IsSmsSent.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Listeners;
+use  App\Models\SMS;
+use App\Events\SendingSms;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class IsSmsSent
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(SendingSms $event)
+    {
+        Log::info("message");
+        DB::table('s_m_s')->insert([
+            'phone'=>$event->phone
+        ]);
+      
+        return response()->json([
+            'success'=>true,
+            'message'=>'Sms Added'
+        ]);
+        Log::info("message");
+    }
+}
